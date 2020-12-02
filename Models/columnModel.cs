a9f5390db7efe54fc14043d47796b12d1f2a4b52
trevorius <trevorius45@hotmail.com>
2020-12-02 06:10:53 -0500

    add column and elevator model

---
+++ Models/columnModel.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerPortal.Models
+{
+
+    public class Column
+    {
+        private DateTime _date = DateTime.Now;
+
+        public long Id { get; set; }
+        public string type_of_building { get; set; }
+        public int number_of_floors_served { get; set; }
+        public string status { get; set; }
+        public string information { get; set; }
+        public string notes { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get {return _date; } set { _date = value; } }
+        public long battery_id { get; set; }
+        public long customer_id { get; set; }
+    }
+
+    // public class ColumnStatus
+    // {
+    //     public long Id { get; set; }
+    //     public string status { get; set; }
+    //     public ColumnStatus(long id, string Status)
+    //     {
+    //         Id = id;
+    //         status = Status;
+    //     }
+        
+    // };
+}
---
+++ Models/elevatorModel.cs
@@ -0,0 +1,37 @@
+using System;
+
+
+namespace CustomerPortal.Models
+{
+        public class Elevator
+    {
+        private DateTime _date = DateTime.Now;
+
+        public long Id { get; set; }
+        public string model { get; set; } 
+        public string type_of_building { get; set; }
+        public string status { get; set; }
+        public DateTime commissioning_date { get; set; }
+        public DateTime last_inspection_date { get; set; }
+        public string inspection_certificate { get; set; }
+        public string information { get; set; }
+        public string notes { get; set; }
+        public DateTime created_at { get; set; }   
+        public DateTime updated_at { get {return _date; } set { _date = value; } }
+        public long column_id { get; set; }
+        public long customer_id { get; set; }
+
+    };
+    public class ElevatorStatus
+    {
+        public long Id { get; set; }
+        public string status { get; set; }
+        public ElevatorStatus(long id, string Status)
+        {
+            Id = id;
+            status = Status;
+        }
+        
+    };
+
+}
