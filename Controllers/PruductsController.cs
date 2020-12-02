ea5b1d0201473e93487e89e2f963d3ae281fa897
trevorius <trevorius45@hotmail.com>
2020-12-02 06:05:21 -0500

    add building model add controller to be filled for products

---
+++ Controllers/PruductsController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CustomerPortal.Models;
+using CustomerPortal.Controllers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CustomerPortal.Controllers
+{
+    public class ProductsController : Controller
+    {
+    }
+}
